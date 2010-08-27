87eb6ca25e3d9407fc54d1c4e0064ef7a5fd5192
Shawn Sprehe <memakeit@gmail.com>
2010-08-27 14:30:54 -0600

    MMI_Jelly api updates

--- classes/model/mmi/customers.php
+++ classes/model/mmi/customers.php
@@ -112,18 +112,17 @@ class Model_MMI_Customers extends Jelly_Model
 	 *
 	 * @param	mixed	one or more id's
 	 * @param	boolean	return the data as an array?
-	 * @param	string	if specified, the key to be used when returning an associative array
 	 * @param	integer	the maximum number of results
 	 * @return	mixed
 	 */
-	public static function select_by_id($ids, $as_array = TRUE, $array_key = NULL, $limit = NULL)
+	public static function select_by_id($id, $as_array = TRUE, $limit = NULL)
 	{
 		$where_parms = array();
-		if (MMI_Util::is_set($ids))
+		if (MMI_Util::is_set($id))
 		{
-			$where_parms['id'] = $ids;
+			$where_parms['id'] = $id;
 		}
 		$query_parms = array('limit' => $limit, 'where_parms' => $where_parms);
-		return MMI_Jelly::select(self::$_table_name, $as_array, $array_key, $query_parms);
+		return MMI_Jelly::select(self::$_table_name, $as_array, $query_parms);
 	}
 } // End Model_MMI_Customers
